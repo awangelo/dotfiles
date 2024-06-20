0f88b75f22f87fef8d75e60471c5168a93c419f8
awangelo <anggg.gaio@gmail.com>
2024-06-20 18:39:33 -0300

    fix: application defaults

--- src/dwm/config.def.h
+++ src/dwm/config.def.h
@@ -27,8 +27,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL,       NULL,       NULL,       0,            False,       -1 },
 };
 
 /* layout(s) */
