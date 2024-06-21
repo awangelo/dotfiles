aff296eb9cf9f35ca0fd413934bb5dd1b1deb82f
awangelo <anggg.gaio@gmail.com>
2024-06-21 12:48:40 -0300

    style: bar alpha

--- .config/src/dwm/config.def.h
+++ .config/src/dwm/config.def.h
@@ -13,7 +13,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 127;
+static const unsigned int baralpha = 153;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
