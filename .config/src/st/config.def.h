04166c697c2a70c516fed84b7fa4764377253df1
awangelo <anggg.gaio@gmail.com>
2024-06-21 19:35:23 -0300

    style: lower st alpha

--- .config/src/st/config.def.h
+++ .config/src/st/config.def.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.88, alphaUnfocused = 0.83;
+float alpha = 0.83, alphaUnfocused = 0.75;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
