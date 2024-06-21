f0c7079a5e8bd9861b8bb2f88dabb9100e8727fb
awangelo <anggg.gaio@gmail.com>
2024-06-21 13:28:56 -0300

    feat: clipmenu command and key

--- .config/src/dwm/config.def.h
+++ .config/src/dwm/config.def.h
@@ -66,9 +66,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *clipmenucmd[] = { "clipmenu", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,					    XK_v,      spawn,          {.v = clipmenucmd },
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,             	        XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
