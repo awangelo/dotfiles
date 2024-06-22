83c14a326ec8c1bcb0e0615709c9b3a5a7faec9c
awangelo <anggg.gaio@gmail.com>
2024-06-22 15:58:32 -0300

    feat: dwmblocks src

---
+++ .config/src/dwmblocks/blocks.def.h
@@ -0,0 +1,11 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "sh $HOME/.config/scripts/mem",	 	5,		0,},
+	{"", "sh $HOME/.config/scripts/battery",	5,		0,},
+	{"", "sh $HOME/.config/scripts/timedate", 	1,		0,},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
