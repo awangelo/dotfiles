e2acd2e056861412f2f122a4db7fbd939a081f15
awangelo <anggg.gaio@gmail.com>
2024-06-22 15:57:15 -0300

    feat: noto fonts for emojis

--- .config/src/dwm/config.def.h
+++ .config/src/dwm/config.def.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 7;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=10" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=10", "Noto Color Emoji:size=10" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
